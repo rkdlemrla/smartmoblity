de085eecca7bfd9e61eb10d992de50778c18cc03
rkdlemrla <rkdlemrla@naver.com>
2024-05-29 17:15:20 +0900

    Add files via upload

--- home.py
+++ home.py
@@ -9,23 +9,33 @@ def bmi_range(bmi):
     elif 23 < bmi <25:
         st.warning("과체중입니다.")
     else:
-        st.warning("저체중입니다.")    
+        st.warning("저체중입니다.")
         
-st.title("체질량지수 계산기")
+selected = st.sidebar.selectbox("목차", ("체질량계산기","갭마인더","국가별통계"))
 
-st.info("체질량지수는 자신의 몸무게를 키의 제곱으로 나눈 값입니다.")
+if selected == "체질량계산기":    
+        
+    st.title("체질량지수 계산기")
 
-height = st.number_input("신장 (cm)", value = 160, step = 3)
-st.write(f"당신의 신장은 : {height} cm")
+    st.info("체질량지수는 자신의 몸무게를 키의 제곱으로 나눈 값입니다.")
 
-weight = st.number_input("체중 (kg)", value = 60, step = 1)
-st.write(f"당신의 체중은 : {weight} kg")
+    height = st.number_input("신장 (cm)", value = 160, step = 3)
+    st.write(f"당신의 신장은 : {height} cm")
 
-bmi = weight / ((height/100)**2)
+    weight = st.number_input("체중 (kg)", value = 60, step = 1)
+    st.write(f"당신의 체중은 : {weight} kg")
 
-if st.button("계산하기"):
-    st.write(f"당신의 체질량지수는 {round(bmi,2)} 입니다.")
-    bmi_range(bmi)
-    st.balloons()
-    
-    st.image('ss.jpeg', caption='균형있는 식단이 필요합니다.')
+    bmi = weight / ((height/100)**2)
+
+    if st.button("계산하기"):
+        st.write(f"당신의 체질량지수는 {round(bmi,2)} 입니다.")
+        bmi_range(bmi)
+        st.balloons()
+        
+        st.image('ss.jpeg', caption='균형있는 식단이 필요합니다.')
+
+if selected == "갭마인더":
+    st.title("갭마인더")
+
+if selected == "국가별통계":
+    st.title("국가별통계")    
